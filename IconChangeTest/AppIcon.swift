12a0d649c982df48a8868d596c1a509b378cce39
Ivan Lé Hjelmeland <ivan.hjelmeland@gmail.com>
2024-06-15 12:31:48 +0200

    Added new model for AppIcon.

---
+++ IconChangeTest/AppIcon.swift
@@ -0,0 +1,14 @@
+//
+//  AppIcon.swift
+//  IconChangeTest
+//
+//  Created by Ivan Lé Hjelmeland on 15/06/2024.
+//  Copyright © 2024 Aboveapps. All rights reserved.
+//
+
+import Foundation
+
+struct AppIcon {
+    let name: String
+    let imageName: String
+}
